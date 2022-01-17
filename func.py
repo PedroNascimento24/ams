37f02bb0ee0afd13bc2a3ba26a528531191863c8
PedroNascimento24 <82335091+PedroNascimento24@users.noreply.github.com>
2022-01-17 23:50:36 +0000

    Register Func Created
    Falta agora o resto das funçoes para os artigos.

--- func.py
+++ func.py
@@ -6,7 +6,7 @@ class DataBase:
         self.order = []
 
     def insertUser(self, data):
-        # UserID | Usernme | Password
+        # UserID | Username | Password
         self.user.append(data)
 
     def insertAdd(self, data):
@@ -32,3 +32,59 @@ class DataBase:
 
     def getOrder(self):
         return self.order.copy()
+
+class Menu:
+    options = {
+        "1": lambda args: Menu._login(args),
+        "2": lambda args: Menu._UserRegister(args),
+        "3": lambda args: exit("See you later"),
+        "a1": lambda args: Menu._GertCart(args),
+        "a2": lambda args: Menu._ListAdd(args),
+        "a3": lambda args: Menu._RegisterAdd(args),
+        "a4": lambda args: exit("See you later"),
+    }
+
+    @staticmethod
+    def _login(db):
+        print("User Login")
+        nome = input('Enter your name:')
+        password = input('Enter your password:')
+
+        for k in db.getuser():
+            if nome in k:
+                if password in k:
+                    print("Successfully logged in.")
+                    return [True, k[0]]
+
+        print("Unsuccessfully logged in.")
+
+    @staticmethod
+    def _UserRegister(db):
+        print("User Register")
+        nome = input('Enter your name:')
+        password = input('Enter your password:')
+
+        for k in db.getuser():
+            if nome in k:
+                Menu._UserRegister()
+
+        db.insertUser([len(db.getuser()) + 1, nome, password])
+        print("Successfully registered.", )
+
+    @staticmethod
+    def _LoginMenu(db):
+        print("+-----------------------------------------------------------------------------------+")
+        print("|                       [1] -> Login                                                |")
+        print("|                       [2] -> Register                                             |")
+        print("|                       [3] -> Exit the program                                     |")
+        print("+-----------------------------------------------------------------------------------+")
+        while True:
+            try:
+                choice = input("What is the option? -> ")
+                if int(choice) > 0 and int(choice) < 4:
+                    return Menu.options[choice](db)
+                else:
+                    print("The value entered is not valid")
+
+            except Exception as e:
+                raise Exception(e)
--- main.py
+++ main.py
@@ -1,1 +1,6 @@
 import func
+
+db = func.DataBase()
+db.insertUser([1, 'admin', 'admin'])
+db.insertUser([2, 'user', 'user'])
+func.Menu._LoginMenu(db)
